f003b0bd59cdfa76333c3f109e499e76d6ad943f
shreyas301197 <shreyas301197@gmail.com>
2023-11-13 14:37:55 -0500

    updated dola.py

--- dola.py
+++ dola.py
@@ -196,11 +196,11 @@ class DoLa:
                     js_divs = 0.5 * (kl1 + kl2)  # shape: (num_premature_layers, batch_size)
 
                     # 6. Reduce the batchmean
-                    js_divs = js_divs.mean(-1)  # shape: (num_premature_layers,)
+                    js_divs = js_divs.mean(-1)  # shape: (num_premature_layers,) -> same premature layer for a token idx across the entire batch
                     premature_layer = candidate_premature_layers[int(js_divs.argmax().cpu().item())]
                     premature_layer_dist[premature_layer] += 1
 
-                    premature_layers.append(premature_layer)
+                    premature_layers.append(premature_layer) # this is the list of selected premature layer for each token idx (across the batch)
 
                 base_logits = torch.zeros_like(dict_outputs[mature_layer][0, prefix_ids.shape[-1] - 1:-1])
                 for i, l in enumerate(premature_layers):
@@ -218,4 +218,72 @@ class DoLa:
                 
                 log_probs = diff_logits[range(diff_logits.shape[0]), continue_ids].sum().item()
 
-        return log_probs, (premature_layer_dist if mode == 'dola' else None)
+        return log_probs, (premature_layer_dist if mode == 'dola' else None), (premature_layers if mode == 'dola' else None)
+    
+    def lm_score_full(self, input_text1, input_text2, pmi=False, max_new_tokens=256, top_p=0.95, top_k=0, temperature=0.8, mature_layer=None, premature_layer=None, candidate_premature_layers=[], mode='baseline', verbose=True, remove_stop_words=False, relative_top=0.1, relative_top_value=-1000.0, post_softmax=True, **kwargs):
+        with torch.no_grad():
+            input_text = input_text1 + input_text2
+            input_ids = self.tokenizer(input_text, return_tensors="pt").input_ids.to(self.device)
+            # prefix_ids = self.tokenizer(input_text1, return_tensors="pt").input_ids.to(self.device)
+            continue_ids = input_ids[0, :]
+            
+
+            if mode == 'dola':
+                premature_layer_dist = {l:0 for l in candidate_premature_layers}
+                picked_logits = []
+                result_dict = {}
+                premature_layers = []
+
+                dict_outputs, outputs = self.model(
+                    input_ids=input_ids,
+                    return_dict=True,
+                    output_attentions=False,
+                    output_hidden_states=False,
+                    early_exit_layers=candidate_premature_layers + [mature_layer],
+                )
+
+                for seq_i in range( input_ids.shape[-1] - 1):
+                    # Pick the less like layer to contrast with
+                    # 1. Stacking all premature_layers into a new dimension
+                    stacked_premature_layers = torch.stack([dict_outputs[i][:, seq_i, :] for i in candidate_premature_layers], dim=0)
+
+                    # 2. Calculate the softmax values for mature_layer and all premature_layers
+                    softmax_mature_layer = F.softmax(dict_outputs[mature_layer][:, seq_i, :], dim=-1)  # shape: (batch_size, num_features)
+                    softmax_premature_layers = F.softmax(stacked_premature_layers, dim=-1)  # shape: (num_premature_layers, batch_size, num_features)
+
+                    # 3. Calculate M, the average distribution
+                    M = 0.5 * (softmax_mature_layer[None, :, :] + softmax_premature_layers)  # shape: (num_premature_layers, batch_size, num_features)
+
+                    # 4. Calculate log-softmax for the KL divergence
+                    log_softmax_mature_layer = F.log_softmax(dict_outputs[mature_layer][:, seq_i, :], dim=-1)  # shape: (batch_size, num_features)
+                    log_softmax_premature_layers = F.log_softmax(stacked_premature_layers, dim=-1)  # shape: (num_premature_layers, batch_size, num_features)
+
+                    # 5. Calculate the KL divergences and then the JS divergences
+                    kl1 = F.kl_div(log_softmax_mature_layer[None, :, :], M, reduction='none').mean(-1)  # shape: (num_premature_layers, batch_size)
+                    kl2 = F.kl_div(log_softmax_premature_layers, M, reduction='none').mean(-1)  # shape: (num_premature_layers, batch_size)
+                    js_divs = 0.5 * (kl1 + kl2)  # shape: (num_premature_layers, batch_size)
+
+                    # 6. Reduce the batchmean
+                    js_divs = js_divs.mean(-1)  # shape: (num_premature_layers,) -> same premature layer for a token idx across the entire batch
+                    premature_layer = candidate_premature_layers[int(js_divs.argmax().cpu().item())]
+                    premature_layer_dist[premature_layer] += 1
+
+                    premature_layers.append(premature_layer) # this is the list of selected premature layer for each token idx (across the batch)
+
+                base_logits = torch.zeros_like(dict_outputs[mature_layer][0, :])
+                for i, l in enumerate(premature_layers):
+                   base_logits[i] = dict_outputs[l][0,  i]
+                final_logits = dict_outputs[mature_layer][0, :]
+                final_logits = final_logits.log_softmax(dim=-1)
+                base_logits = base_logits.log_softmax(dim=-1)
+                diff_logits = final_logits - base_logits
+                if post_softmax:
+                    diff_logits = diff_logits.log_softmax(dim=-1)
+
+                if relative_top > 0.0:
+                    relative_top_mask = self.get_relative_top_filter(final_logits, relative_top)
+                    diff_logits = torch.where(relative_top_mask, relative_top_value, diff_logits)
+                
+                log_probs = diff_logits[range(diff_logits.shape[0]), continue_ids].sum().item()
+
+        return log_probs, (premature_layer_dist if mode == 'dola' else None), (premature_layers if mode == 'dola' else None)
